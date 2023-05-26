2331c5a64d236b80c5759ae8621c65ab5f30fd6b
ericz02 <ezheng0720@gmail.com>
2023-05-25 23:22:43 -0400

    maximum subarray

---
+++ 53-maximum-subarray.py
@@ -0,0 +1,11 @@
+def maxSubArray(nums):
+    if not nums:
+        return 0
+
+    current_sum = max_sum = nums[0]
+
+    for i in range(1, len(nums)):
+        current_sum = max(nums[i], current_sum + nums[i])
+        max_sum = max(max_sum, current_sum)
+
+    return max_sum
