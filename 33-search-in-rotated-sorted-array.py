cf30d746cbc998fa7d382eebee9c9236663c8365
ericz02 <ezheng0720@gmail.com>
2023-05-28 23:37:18 -0400

    adds comment

--- 33-search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array.py
@@ -1,22 +1,22 @@
 def search(nums, target):
-    left = 0
-    right = len(nums) - 1
+    left = 0  # Initialize the left pointer
+    right = len(nums) - 1  # Initialize the right pointer
 
-    while left <= right:
-        mid = (left + right) // 2
+    while left <= right:  # Perform binary search until left exceeds right
+        mid = (left + right) // 2  # Calculate the middle index
 
-        if nums[mid] == target:
+        if nums[mid] == target:  # Check if the middle element is the target
             return mid
 
-        if nums[left] <= nums[mid]:
-            if nums[left] <= target < nums[mid]:
-                right = mid - 1
+        if nums[left] <= nums[mid]:  # If the left half is sorted
+            if nums[left] <= target < nums[mid]:  # Check if the target is within the left half
+                right = mid - 1  # Update the right pointer to search in the left half
             else:
-                left = mid + 1
-        else:
-            if nums[mid] < target <= nums[right]:
-                left = mid + 1
+                left = mid + 1  # Update the left pointer to search in the right half
+        else:  # If the right half is sorted
+            if nums[mid] < target <= nums[right]:  # Check if the target is within the right half
+                left = mid + 1  # Update the left pointer to search in the right half
             else:
-                right = mid - 1
+                right = mid - 1  # Update the right pointer to search in the left half
 
-    return -1
+    return -1  # Return -1 if the target element is not found
